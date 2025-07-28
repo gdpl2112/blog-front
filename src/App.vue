674479bdd79bc6ffeb34c9fe2c4c6ba69ac4900c
Kloping <3474006766@qq.com>
2025-07-28 21:27:34 +0800

    chore: 更新背景图片路径为/f目录下资源

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <style>
 body {
   margin: 0;
-  background: #333 url("/pc.jpg") no-repeat fixed center/cover;
+  background: #333 url("/f/pc.jpg") no-repeat fixed center/cover;
   min-height: 100vh;
 }
 
 @media (max-width: 768px) {
   body {
-    background-image: url("/pe.jpg");
+    background-image: url("/f/pe.jpg");
     background-size: 100% auto;
   }
 }
