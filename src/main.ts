0df06f12384d006aee407ee10beb9a79f3eb0af1
Kloping <3474006766@qq.com>
2025-08-08 22:27:40 +0800

    build(dependencies): 更新项目依赖并调整导入顺序
    - 更新 ElementPlus 版本至 2.10.6
    - 更新 axios版本至 ^1.7.7
    - 更新 vite 版本至 6.3.4
    - 新增 unplugin-auto-import依赖
    - 调整 main.ts 中的导入顺序

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 // main.ts
 import {createApp} from 'vue'
 
-import App from './App.vue'
-
-import 'virtual:windi.css'
-import router from './router'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import "bootstrap/dist/js/bootstrap.bundle.min"
-
-import 'aplayer/dist/APlayer.min.css';
 import ElementPlus from 'element-plus'
 
 import 'element-plus/dist/index.css'
 
-import('./perm')
+import App from './App.vue'
+
+import 'virtual:windi.css'
+import '@/perm'
+import router from './router'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+import 'aplayer/dist/APlayer.min.css';
 
 const app = createApp(App)
 
