07ac169cef1fbff7b24d2782b75a3b03266acef0
Kloping <3474006766@qq.com>
2025-08-08 15:39:24 +0800

    build(dependencies): 更新项目依赖并调整组件注册方式
    - 移除 unplugin-auto-import 和 unplugin-vue-components 插件
    - 手动导入 ElementPlus 组件库
    - 更新 package.json 中的依赖版本
    - 调整 main.ts 中的组件注册方式

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import {createApp} from 'vue'
 import App from './App.vue'
 
 import 'virtual:windi.css'
-import('./perm')
 import router from './router'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,10 +11,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min"
 
 import 'aplayer/dist/APlayer.min.css';
+import ElementPlus from 'element-plus'
+
+import 'element-plus/dist/index.css'
+
+import('./perm')
 
 const app = createApp(App)
 
-// app.use(ElementPlus)
+app.use(ElementPlus)
 
 app.use(router)
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,34 +7,13 @@ import WindiCSS from 'vite-plugin-windicss'
 
 import path from 'path'
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
 let baseUrl = 'http://127.0.0.1:81/'
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        minify: 'esbuild', // 比 terser 快 10 倍[2](@ref)
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // 将大依赖拆分为独立 chunk
-                        if (id.includes('element-plus')) return 'vendor-element';
-                        if (id.includes('bootstrap')) return 'vendor-bootstrap';
-                        return 'vendor';
-                    }
-                }
-            }
-        }
-    },
     base: './',
     plugins: [
         vue(),
         WindiCSS(),
-        AutoImport({ resolvers: [ElementPlusResolver()] }),
-        Components({ resolvers: [ElementPlusResolver()] }),
     ],
     resolve: {
         alias: {
